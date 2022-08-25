7b593c219a063736aa84868fbd6e2fdf7a39de17
Oussama Ait Manssour <67986338+AMOussama@users.noreply.github.com>
2022-08-25 13:09:32 +0100

    Update gg_lang.php

--- source/gg_lang.php
+++ source/gg_lang.php
@@ -1,3 +1,5 @@
 <?php
-echo file_get_contents("fr_lang.php");
+$myfile = fopen("fr_lang.php", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("fr_lang.php"));
+fclose($myfile);
 ?>
